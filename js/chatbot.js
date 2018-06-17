9490f2e55d8b081b1809f3a51f313628175a6c96
Luca Crippa <luca.crippa88@gmail.com>
2018-06-17 18:27:00 +0200

    Bugfix and optimization

--- js/chatbot.js
+++ js/chatbot.js
@@ -113,10 +113,12 @@ function getWatsonAssistantData(context){
 function createHead(){
 	var met_sta = ["<meta http-equiv='X-UA-Compatible' content='IE=edge'><meta name='viewport' content='width=device-width, initial-scale=1.0'><meta name='ROBOTS' content='INDEX,FOLLOW'><meta name='GOOGLEBOT' content='ARCHIVE'>"];
 	var css_ico = ["<link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons'>"]; // css per Material Design Icons
-	var css_def = ["<link rel='stylesheet' type='text/css' href='css/default.css'/>"]; // css default (reset)
+  var css_car = ["<link rel='stylesheet' type='text/css' href='css/carbon-components.min.css'/>"]; // css carbon design
+  var js_car  = ["<link rel='stylesheet' type='text/css' href='js/carbon-components.min.js'/>"]; // js carbon design
+  var css_def = ["<link rel='stylesheet' type='text/css' href='css/default.css'/>"]; // css default (reset)
 	var css_mat = ["<link rel='stylesheet' type='text/css' href='css/material-design.css' media='screen,projection'/>"]; // css per Material Design
 	var title = ["<title>Watson Assistant in PHP</title>"];
-	var headfull = met_sta.concat(css_ico, css_def, css_mat, title);
+	var headfull = met_sta.concat(css_ico, css_car, js_car, css_def, css_mat, title);
 	$('head').append(headfull);
 }
 
