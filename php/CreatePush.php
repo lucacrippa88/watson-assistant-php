f41a154fce7d4778253dc89e41d5a12b7b07f8fa
Luca Crippa <luca.crippa88@gmail.com>
2018-10-17 20:55:33 +0200

    Created CreatePush.php

--- php/CreatePush.php
+++ php/CreatePush.php
@@ -1,15 +1,59 @@
 <?php
 /*
- * API: CreatePush ©
- * Features: creates push notification
- * References: https://www.ibm.com/watson/developercloud/assistant/api/v1/
- * Author: Luca Crippa - luca.crippa88@gmail.com
- * Date: April 2018
+ * API: SendPush ©
+ * Returns: sends push notification using OneSignal
+ * Requires: OneSignal app_id & auth key
+ * Author: OneSignal, Luca Crippa - luca.crippa88@gmail.com
+ * Reference: https://documentation.onesignal.com/reference#create-notification
+ * Date: July 2018
  */
 
+function sendMessage() {
 
+  $title = addslashes($_POST['title']);
+  $text = addslashes($_POST['text']);
+  $url = addslashes($_POST['url']);
+  $segment = addslashes($_POST['segment']);
 
+    $content = array("en" => $text);
+    $headings = array("en" => $title);
+    $hashes_array = array();
 
+    $fields = array(
+        'app_id' => "9632c98e-57ff-4279-a593-b3360dbadcc1",
+        'included_segments' => array($segment),
+        'contents' => $content,
+        'headings' => $headings,
+        'url' => $url,
+    );
+
+    $fields = json_encode($fields);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json; charset=utf-8',
+        'Authorization: Basic ZTk4MDg1ZjUtMjQ2Zi00ZTQyLWE3YTUtMDFiYWVkNzJiZjZl'
+    ));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+    curl_setopt($ch, CURLOPT_HEADER, FALSE);
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return $response;
+}
+
+$response = sendMessage();
+
+$array['Response'] = array(
+  'sent' => 'ok',
+);
+
+echo json_encode($array); // Returns confirmation
 
 
 ?>
