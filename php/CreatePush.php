315a394fbc978964038673a73da1c1c72a3f6295
Luca Crippa <luca.crippa88@gmail.com>
2018-10-17 19:51:24 +0200

    Created pho createpush

---
+++ php/CreatePush.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * API: CreatePush ©
+ * Features: creates push notification
+ * References: https://www.ibm.com/watson/developercloud/assistant/api/v1/
+ * Author: Luca Crippa - luca.crippa88@gmail.com
+ * Date: April 2018
+ */
+
+
+
+
+
+
+?>
