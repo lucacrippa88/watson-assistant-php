50458589f418bdb4b225bbcdb46f8358d6730af4
Luca Crippa <luca.crippa88@gmail.com>
2018-10-18 18:27:11 +0200

    Comments added

--- php/CreatePush.php
+++ php/CreatePush.php
@@ -33,7 +33,7 @@ function sendMessage() {
     curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
         'Content-Type: application/json; charset=utf-8',
-        'Authorization: Basic insert-rest-api-key-here'
+        'Authorization: Basic insert-rest-api-key-here' // INSERT API KEY HERE
     ));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
     curl_setopt($ch, CURLOPT_HEADER, FALSE);
