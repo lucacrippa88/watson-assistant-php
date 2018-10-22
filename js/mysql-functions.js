240edb0574177d017dfc31ef5ffbd8c6a9e73e59
Luca Crippa <luca.crippa88@gmail.com>
2018-10-22 15:30:03 +0200

    Bugfix

--- js/mysql-functions.js
+++ js/mysql-functions.js
@@ -13,9 +13,7 @@
  */
 function saveDataWA(postdata) {
 
-    var seltitle = selectRandomTitle(); // select random title (+ image for confirmation swal: not working!)
-
-    var api = 'api/UpdateDB.php'; // connects and updates the selected DB table
+    var api = 'link-to-your-du-update-api'; // connects and updates the selected DB table
 
     $.ajax({
       url: api,
@@ -59,7 +57,7 @@ function saveDataWA(postdata) {
         // DB updated!
         else if (obj.updated == "ok") {
           swal({
-            title: seltitle.title,
+            title: 'Contenuto pubblicato!',
             html: 'Watson Assistant ha salvato il post in bozza.<br><br>',
             // type: 'success',
 						imageUrl: 'img/watson.png',
@@ -82,93 +80,5 @@ function saveDataWA(postdata) {
 
       } // end of function
     }); // end of ajax call
-
-}
-
-
-/*
- * Function: publishData Â©
- * Returns: lettura e invio dati da pagina di creazione/edit di post verso API per update del DB in stato di post pubblicato
- * Author: Luca Crippa - luca.crippa88@gmail.com
- * Date: July 2017
- */
-function publishData(id) {
-  $("#publish-post").click(function() {
-
-    var post_id = id; // get id of post: if numeric, edit post; if not numeric (null or something else), create a new post
-
-		var postdata;
-		postdata = getAllData(id); // get all data from admin create/edit post page
-
-    postdata.public = true; // set publish (instead of save draft)
-    postdata.id = post_id; // insert id to postdata
-
-    var seltitle = selectRandomTitle(); // select random title (+ image for confirmation swal: not working!)
-
-    var api = 'api/UpdateDB.php'; // connects and updates the selected DB table
-
-    $.ajax({
-      url: api,
-      type: 'POST',
-      data: postdata,
-      dataType: 'json',
-      async: true,
-      success: function(result) {
-        var obj = result.Response;
-
-        // Check connection
-        if (obj.connected != "ok") {
-          swal({
-            title: 'Errore connessione DB',
-            html: obj.connected + '<br><br>',
-            customClass: 'dialog',
-            buttonsStyling: false,
-            confirmButtonClass: 'bx--btn bx--btn--sm bx--btn--primary',
-            confirmButtonText: 'Ok',
-            type: 'error'
-          }).then(function() {
-            window.location.replace("/" + admin_page + "/" + list_page) // redirect after clicking "ok"
-          })
-        }
-
-        // Check update
-        else if (obj.updated != "ok") {
-          swal({
-            title: 'Errore update DB',
-            html: obj.updated + '<br><br>',
-            customClass: 'dialog',
-            buttonsStyling: false,
-            confirmButtonClass: 'bx--btn bx--btn--sm bx--btn--primary',
-            confirmButtonText: 'Ok',
-            type: 'error',
-          }).then(function() {
-            window.location.replace("/" + admin_page + "/" + list_page) // redirect after clicking "ok"
-          })
-        }
-
-        // DB updated!
-        else if (obj.updated == "ok") {
-          swal({
-            title: seltitle.title,
-            html: 'Il post &egrave; pubblico e visibile da tutti gli utenti del sito.<br><br>',
-            type: 'success',
-            showCloseButton: false,
-            showCancelButton: false,
-            showConfirmButton: true,
-            buttonsStyling: false,
-            animation: false,
-            customClass: 'dialog',
-            confirmButtonClass: 'bx--btn bx--btn--sm bx--btn--primary',
-            confirmButtonText: 'Ok',
-            allowOutsideClick: false // prevent user by clicking outside instead of clickinf "ok"
-          }).then(function() {
-            window.location.replace("/" + admin_page + "/" + list_page) // redirect after clicking "ok"
-          })
-        }
-
-      } // end of function
-    }); // end of ajax call
-
-  });
 
 }
